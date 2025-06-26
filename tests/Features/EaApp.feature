0afb9a64d6cc518df5034a744934f56fc7b3d1db
Devendiran7 <devendiran7699@gmail.com>
2025-06-26 15:51:36 +0530

    Updated

--- tests/Features/EaApp.feature
+++ tests/Features/EaApp.feature
@@ -9,12 +9,24 @@ Feature: EaApp automation
       | UserName | password |
       | admin    | password |
 
-@Invalid
-  Scenario Outline: Validate Emp Details 
+  @Invalid
+  Scenario Outline: Validate Emp Details
     Given User launch the EAApp application URL
     When User login with valid "<UserName>" and "<password>"
     Then User Validates the Invalid Credentials error message
 
     Examples:
-      | UserName | password |
+      | UserName | password    |
       | admin1   | password231 |
+
+  @EmpDetailValidation
+  Scenario Outline: Validate Employee Details
+    Given User launch the EAApp application URL
+    When User login with valid "<UserName>" and "<password>"
+    And User Navigate to Employee Details page
+    Then User Validates the employee details "<empName>","<empSalary>" and "<empGrade>"
+
+    Examples:
+      | UserName | password | empName | empSalary | empGrade |
+      | admin    | password | John    | 2500      | 3        |
+
--- tests/StepDefinition/EaApp.js
+++ tests/StepDefinition/EaApp.js
@@ -1,12 +1,12 @@
-import { Given, When, Then, After, Before, DataTable,setDefaultTimeout } from '@cucumber/cucumber';
+import { Given, When, Then, After, Before, DataTable, setDefaultTimeout } from '@cucumber/cucumber';
 import { test, expect, chromium } from '@playwright/test';
 import { after } from 'node:test';
 // import { loginPage } from '../../PageObject/Login.js'; 
 setDefaultTimeout(60 * 1000);
 let browser, page;
 
-Before(async function() {
-     browser = await chromium.launch({ headless: false });
+Before(async function () {
+    browser = await chromium.launch({ headless: false });
     const context = await browser.newContext();
     page = await context.newPage();
 });
@@ -22,9 +22,24 @@ Then(`User login with valid {string} and {string}`, async function (UserName, pa
     await page.getByRole('textbox', { name: 'Password' }).fill(password);
     await page.getByRole('button', { name: 'Log in' }).click();
     // await expect(page.getByRole('link', { name: 'Log off' })).toBeVisible();
+    await expect(await page.getByRole('link', { name: 'Hello admin!' })).toBeVisible();
+
 });
-Then(`User Validates the Invalid Credentials error message`, async () =>{   
-  await expect(page.getByText('Invalid login attempt.')).toBeVisible();
+Then(`User Validates the Invalid Credentials error message`, async () => {
+    await expect(page.getByText('Invalid login attempt.')).toBeVisible();
+});
+
+When(`User Navigate to Employee Details page`, async () => {
+    await page.getByRole('link', { name: 'Employee Details' }).click();
+});
+
+Then(`User Validates the employee details {string},{string} and {string}`, async (empName, empSalary, empGrade) => {
+
+    await page.getByRole('link', { name: 'Employee Details' }).click();
+    await expect(await page.getByRole('cell', { name: 'John', exact: true }).textContent()).toContain(empName);
+    await expect(await page.getByRole('cell', { name: '2500' }).textContent()).toContain(empSalary);
+    await expect(await page.getByRole('row', { name: 'John 2500 3 EmployeePF |' }).getByRole('cell').nth(2).textContent()).toContain(empGrade);
+    await console.log("******* Success *******");
 });
 
 After(async function () {
